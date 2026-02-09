471bc48a4eeae731765f20e79994d42351bed29d
milos <milos@danceresource.org>
2026-02-09 10:59:09 +0000

    Remove debug sidebar output

--- src/HookHandler.php
+++ src/HookHandler.php
@@ -9,14 +9,6 @@ use Title;
 
 class HookHandler {
 	public static function onSkinBuildSidebar( $skin, &$bar ): bool {
-		if ( $skin->getRequest()->getCheck( 'debugsidebar' ) ) {
-			$bar['navigation'] = $bar['navigation'] ?? [];
-			array_unshift( $bar['navigation'], [
-				'text' => 'AiTranslationExtension hook active',
-				'href' => '#',
-			] );
-		}
-
 		$config = MediaWikiServices::getInstance()->getMainConfig();
 		if ( !$config->get( 'AiTranslationExtensionLanguageSidebar' ) ) {
 			return true;
@@ -29,7 +21,6 @@ class HookHandler {
 		}
 
 		$text = '';
-		$selectedCode = null;
 		foreach ( $candidates as $code ) {
 			$title = Title::newFromText( "MediaWiki:Sidebar/$code" );
 			if ( $title && $title->exists() ) {
@@ -41,7 +32,6 @@ class HookHandler {
 					$candidate = ContentHandler::getContentText( $content );
 					if ( trim( $candidate ) !== '' ) {
 						$text = $candidate;
-						$selectedCode = $code;
 						break;
 					}
 				}
@@ -54,18 +44,6 @@ class HookHandler {
 
 		$bar = [];
 		$skin->addToSidebarPlain( $bar, $text );
-		if ( $skin->getRequest()->getCheck( 'debugsidebar' ) ) {
-			$debugItem = [
-				'text' => "Sidebar lang={$lang} candidates=" . implode( ',', $candidates ) .
-					" selected={$selectedCode} textlen=" . strlen( $text ),
-				'href' => '#',
-			];
-			if ( isset( $bar['navigation'] ) && is_array( $bar['navigation'] ) ) {
-				array_unshift( $bar['navigation'], $debugItem );
-			} else {
-				$bar['navigation'] = [ $debugItem ];
-			}
-		}
 		return false;
 	}
 }
